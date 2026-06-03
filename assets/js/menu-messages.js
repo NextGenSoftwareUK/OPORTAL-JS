9fa316671a5fbf5e8fb4e8db567a1457f376fc5e
David Ellams <davidellams@hotmail.com>
2026-06-03 15:35:46 -0500

    moved last of js out of the shell

--- assets/js/menu-messages.js
+++ assets/js/menu-messages.js
@@ -1,4 +1,42 @@
 (function () {
+  function setup() {
+    var user;
+    var avatarRaw = localStorage.getItem('avatar');
+    if (avatarRaw && avatarRaw !== 'undefined') {
+      try {
+        user = JSON.parse(avatarRaw);
+      } catch (error) {
+        user = null;
+      }
+    }
+
+    var avatarDiv = document.querySelector('.nav__item--account');
+    var icon = document.getElementsByClassName('avatar')[0];
+
+    if (!avatarDiv || !icon) return;
+
+    if (localStorage.getItem('loggedIn') === 'true') {
+      var guestLinks = document.getElementById('guest-links');
+      var username = document.getElementById('username');
+
+      if (guestLinks) {
+        guestLinks.style.display = 'none';
+      }
+      avatarDiv.classList.add('loggedin');
+
+      if (username && user && user.username) {
+        username.innerHTML = user.username;
+      }
+
+      icon.src = 'assets/img/loggedin.png';
+    } else {
+      if (avatarDiv.classList.contains('loggedin')) {
+        avatarDiv.classList.remove('loggedin');
+      }
+      icon.src = 'assets/img/loggedout.png';
+    }
+  }
+
   function showMenuMessage(title, message) {
     var panel = document.getElementById('menu-message');
     if (!panel) return;
@@ -49,4 +87,15 @@
   window.showSTARMessage = showSTARMessage;
   window.showOurWorldMessage = showOurWorldMessage;
   window.goToAvatar = goToAvatar;
+  window.setup = setup;
+
+  function init() {
+    setup();
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
 })();
